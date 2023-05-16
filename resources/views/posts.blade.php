787938520c71421f4379655aee30fa58d0d76052
unknown <petarpetrevski99@gmail.com>
2023-05-16 17:00:08 +0200

    started to convert html and css to blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,25 @@
 <x-layout>
-    @foreach ($posts as $post)
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+
+        <x-post-featured-card :post="$posts[0]" />
+
+        @if ($posts->count() > 1)
+            <div class="lg:grid lg:grid-cols-2">
+                @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post"/>
+                @endforeach
+            </div>
+        @endif
+
+        @else
+            <p class="text-center">No posts yet.</p>
+        @endif
+    </main>
+
+    {{-- @foreach ($posts as $post)
     <article>
         <h1>
             <a href="/post/{{ $post->slug }}">
@@ -24,5 +44,5 @@
             {{ $post->excerpt }}
         </div>
     </article>
-    @endforeach
+    @endforeach --}}
 </x-layout>
