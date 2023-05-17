311e2c3a0bddfb7983d745cc04d5cbe68ae07ef4
unknown <petarpetrevski99@gmail.com>
2023-05-17 14:29:02 +0200

    configured girds, converted post page to blade and extracted dropdown to a component

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,10 @@ Route::get('/', function () {
         // 'posts' => Post::latest()->with('category', 'author')->get()
 
         // fixing using with property in the model
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('/post/{post:slug}', function (Post $post) {
     return view('post', [
@@ -41,12 +42,15 @@ Route::get('/post/{post:slug}', function (Post $post) {
 Route::get('/category/{category:slug}', function (Category $category) {
     return view('posts', [
         // 'posts' => $category->posts->load(['category', 'author']) // eager loading, other method is in the model using with property
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('/author/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
