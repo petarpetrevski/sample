e758d998efc4b735570e5f35c6c1fc3854acd054
petarpetrevski <petarpetrevski99@gmail.com>
2023-05-21 20:45:20 +0200

    validate and store post thumbnails using symbolic link

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,11 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
-            <form action="/admin/post" method="POST" class="mt-10">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
+
+        <x-panel>
+            <form action="/admin/post" method="POST" enctype="multipart/form-data" class="mt-10">
                 @csrf
 
                 <div class="mb-6">
@@ -38,6 +42,24 @@
                     >
 
                     @error('slug')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                    for="thumbnail"
+                        >Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                            type="file"
+                            name="thumbnail"
+                            id="thumbnail"
+                            required
+                    >
+
+                    @error('thumbnail')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
                 </div>
